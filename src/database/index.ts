74a71004b215312d3fed1a3cf953dd672dff7f01
Elioenay <elionay407@gmail.com>
2022-11-10 23:36:53 -0300

    first commit

---
+++ src/database/index.ts
@@ -0,0 +1,17 @@
+import mysql from 'mysql2'
+
+export const database = () => {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'cars',
+    user: 'root',
+    password: 'password',
+  })
+
+  connection.connect(e => {
+    if (e) throw e.message
+    console.log('Database conected')
+  })
+
+  return connection
+}
